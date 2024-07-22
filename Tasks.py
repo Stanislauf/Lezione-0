641380f27ec7c1beeb65ff17ec5b8b765f901b4d
Stanislauf <psv88877@gmail.com>
2024-07-22 12:19:31 +0300

    Add files via upload

---
+++ Tasks.py
@@ -0,0 +1,10 @@
+# 1st program
+print ((9**0.5) * 5)
+# 2st program
+print (9.99 > 9.98 and 1000 != 1000.1)
+# 3st program
+print (1234, 5678)
+print ((1234 % 1000) // 10 + (5678 % 5000) // 10)
+# 4st program
+print (13.42, 42.13)
+print(int(13.42) == int(42.13 * 100) % 4200 or int(42.13) == int(13.42 * 100) % 1300)
